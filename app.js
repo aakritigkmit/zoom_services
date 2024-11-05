06664e16073b2b9f242e113342e1e15880f10a2f
aakritigkmit <aakriti@gkmit.co>
2024-11-05 17:04:25 +0530

    feat:[zc-3] db connection with postgres using sequelize

--- app.js
+++ app.js
@@ -1,11 +1,26 @@
 
 const express = require('express');
+const {sequelize}=require('./src/models');
+const dotenv=require('dotenv')
 
+dotenv.config();
 
+const connectDb=async function(){
+    try{
+        await sequelize.authenticate();
+        console.log("Database connected successfully")
+    }
 
+        catch(error){
+            console.log("error generated while connected to databse",error)
+       }
+}
+
+connectDb();
 
 const app = express();
 app.use(express.json());
+
 
 app.get('/',(req, res)=>{
     res.send('Hello world');
