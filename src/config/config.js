5c98f82631143c8137f7cd768403ed2ec22a3830
aakritigkmit <aakriti@gkmit.co>
2024-11-07 10:45:32 +0530

    chore:fix config for database

--- src/config/config.js
+++ src/config/config.js
@@ -5,7 +5,7 @@ module.exports = {
   development: {
     username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE,
+    database: process.env.DATABASE,
     host: process.env.HOST,
     dialect: process.env.DB_DIALECT,
   },
